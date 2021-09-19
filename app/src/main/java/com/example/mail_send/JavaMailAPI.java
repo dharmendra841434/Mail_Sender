20ac09c3769d58e1d8a1ffefe3e18f91c51dba4f
Dharmendra Kumar <48255952+dharmendra841434@users.noreply.github.com>
2021-09-19 16:28:29 +0530

    Update JavaMailAPI.java

--- app/src/main/java/com/example/mail_send/JavaMailAPI.java
+++ app/src/main/java/com/example/mail_send/JavaMailAPI.java
@@ -15,12 +15,6 @@ import javax.mail.internet.MimeMessage;
 
 public class JavaMailAPI extends AsyncTask<Void,Void,Void>  {
 
-    //Add those line in dependencies
-    //implementation files('libs/activation.jar')
-    //implementation files('libs/additionnal.jar')
-    //implementation files('libs/mail.jar')
-
-    //Need INTERNET permission
 
     //Variables
     private Context mContext;
